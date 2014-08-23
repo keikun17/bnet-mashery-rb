0d579d3f3efc07c224eeb65e02d7b3a17c97e83c
Buddy Magsipoc <keikun17@gmail.com>
2014-08-23 18:44:58 +0800

    Gem skeleton。

---
+++ lib/bnet/mashery/rb2.rb
@@ -0,0 +1,9 @@
+require "bnet/mashery/rb2/version"
+
+module Bnet
+  module Mashery
+    module Rb2
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bnet/mashery/rb2/version.rb
@@ -0,0 +1,7 @@
+module Bnet
+  module Mashery
+    module Rb2
+      VERSION = "0.0.1"
+    end
+  end
+end
