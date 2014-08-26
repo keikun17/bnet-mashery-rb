eb7408494b36611f0502f7db2de5937bde6bc4e4
Buddy Magsipoc <keikun17@gmail.com>
2014-08-26 19:58:37 +0800

    VCR and Rspec colors, milord。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,19 @@ Coveralls.wear! #  Load this ffirst before the tested library
 
 require 'pry'
 require 'mashery'
+require 'vcr'
+require 'ostruct'
 
+VCR.configure do |vcr|
+  vcr.cassette_library_dir = 'fixtures/cassettes'
+  vcr.default_cassette_options = {record: :once}
+  vcr.configure_rspec_metadata!
+  vcr.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.failure_color = :magenta
+  # optional config here
+end
