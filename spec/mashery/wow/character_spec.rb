5bfdb9eb5c3b49bbcae6cb9f9a71b68ce977c9cc
Buddy Magsipoc <keikun17@gmail.com>
2014-08-30 23:15:14 +0800

    WoW Character 'from_api' spec and implementation, Milord.

--- spec/mashery/wow/character_spec.rb
+++ spec/mashery/wow/character_spec.rb
@@ -3,7 +3,29 @@ require 'spec_helper'
 describe Mashery::WOW::Character do
 
   describe ".from_api" do
-    it "is initialized"
+    let(:attrs) {
+      {
+        "name" => 'Alexeistukov',
+        "realm" => 'Dalvengyr',
+        "battlegroup" => 'somebg',
+        "class" => 3,
+        "race" => 9,
+        "gender" => 1,
+        "level" => 85,
+        "achievement_points" => 1000,
+        "calc_class" => "b'",
+        "total_honorable_kills" => 100
+      }
+    }
+    subject{ described_class.from_api(attrs)}
+
+    it "initializes and instance when passed with mashery json args" do
+      expect(subject).to be_a_kind_of(described_class)
+      expect(subject.name).to eq("Alexeistukov")
+      expect(subject.level).to eq(85)
+      expect(subject.realm).to eq('Dalvengyr')
+      #TODO assigns :gender, :class, :race
+    end
   end
 
   describe ".find" do
