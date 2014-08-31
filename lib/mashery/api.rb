4b51ec8657c9d5b1f185cf1b61583caf3871b966
Buddy Magsipoc <keikun17@gmail.com>
2014-08-31 23:05:47 +0800

    Tag for removal : API's :secret, Milord.

--- lib/mashery/api.rb
+++ lib/mashery/api.rb
@@ -1,5 +1,7 @@
 module Mashery
   class API
+    # FIXME : Remove :secret and get the API to append
+    # the :key param to the end of each 'get' call
     attr_accessor :region, :secret, :key
 
     def initialize args
