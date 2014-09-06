c4a817631570e7a0a39906225759325497ae2f1a
Buddy Magsipoc <keikun17@gmail.com>
2014-09-07 01:05:16 +0800

    Follower spec and implementation, milord.

--- lib/bnet/diablo3/follower.rb
+++ lib/bnet/diablo3/follower.rb
@@ -1,7 +1,21 @@
-class Bnet::Diablo3::Follower
+class Bnet::Diablo3::Follower < Bnet::BnetResource
   attr_accessor :follower_type, :level, :raw_attributes,
     :magic_find, :gold_find, :experience_bonus,
 
     #associations
     :items, :skills
+
+  def self.from_api(follower_type, raw_response)
+    follower = new(follower_type: follower_type)
+    follower.raw_attributes = raw_response
+    follower.level = raw_response["level"]
+    # follower.items = raw_response["items"]
+    # follower.skills = raw_response["skills"]
+    follower.magic_find = raw_response["stats"]["magicFind"]
+    follower.gold_find = raw_response["stats"]["goldFind"]
+    follower.experience_bonus =  raw_response["stats"]["experienceBonus"]
+
+    return follower
+  end
+
 end
--- spec/bnet/diablo3/follower_spec.rb
+++ spec/bnet/diablo3/follower_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 describe Bnet::Diablo3::Follower do
 
+  describe('.from_api(follower_type, raw_response)') do
+    subject {described_class.from_api(follower_type, raw_response)}
+    let(:follower_type) {'templar'}
+    let(:raw_response) do
+      {'level' => 69, 'stats' => {
+        'magicFind' => 50,
+        'experienceBonus' => 40,
+        'goldFind' => 30,
+      }}
+    end
+
+    it "should be initialized" do
+      expect(subject).to be_an_instance_of(described_class)
+      expect(subject).to have_attributes(
+        {
+          follower_type: 'templar',
+          level: 69,
+          magic_find: 50,
+          experience_bonus: 40,
+          gold_find: 30
+        }
+      )
+    end
+  end
 end
