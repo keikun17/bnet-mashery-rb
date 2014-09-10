4f40c8864c41ce71f7a3235e48f0606e92396a61
Buddy Magsipoc <keikun17@gmail.com>
2014-09-11 01:02:46 +0800

    Sc2 profile previous ladder season pending spec, Milord.

--- spec/bnet/starcraft2/profile_spec.rb
+++ spec/bnet/starcraft2/profile_spec.rb
@@ -91,11 +91,17 @@ describe Bnet::Starcraft2::Profile do
       let(:profile) do
         Bnet::Starcraft2::Profile.find(profile_id: 2144359, name: 'JaBiTo', region: 'us')
       end
-      it "returns the ladders statistics for the profile" do
+      it "returns the current ladders statistics for the profile" do
         expect(subject).to_not be_empty
         expect(subject).to include( an_object_having_attributes(ladder_name: 'Arbiter Epsilon'))
         expect(subject[0].characters[0]['displayName']).to eq('JaBiTo')
       end
+    end
+  end
+
+  describe "#previous_ladder_season" do
+    context "Jabito's ladder records user are found", vcr: {cassette_name: "SC2 Jabito's Ladder Records" } do
+      it "returns the previous ladder statisttics for the profile"
     end
   end
 end
